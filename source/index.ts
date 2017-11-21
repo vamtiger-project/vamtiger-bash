20fe0f0f779ec7acc0714a2d72e69da40c7c6d20
James-Alex Matthews <vamtiger@vamtiger.com>
2017-11-21 08:27:31 +0200

    Mergex in from source

--- source/index.ts
+++
@@ -1,6 +0,0 @@
-import { exec, ExecOptions} from 'child_process';
-import * as BlueBird from 'bluebird';
-
-export type VamtigerBash = (script: string, options?: ExecOptions) => BlueBird<string>;
-
-export default BlueBird.promisify(exec) as VamtigerBash;
