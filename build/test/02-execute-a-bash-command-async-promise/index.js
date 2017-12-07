8a45f28ec89520fe51858ccfbe18a9601762a73a
James-Alex Matthews <vamtiger@vamtiger.com>
2017-12-08 00:24:22 +0200

    vamtiger-node-typescript-commit

--- build/test/02-execute-a-bash-command-async-promise/index.js
+++ build/test/02-execute-a-bash-command-async-promise/index.js
@@ -13,6 +13,8 @@ const __1 = require("../..");
 describe('vamtiger-bash should', function () {
     it('execute a bash command as a promise', function () {
         return __awaiter(this, void 0, void 0, function* () {
+            let result;
+            let directoryContents;
             const listDirectoryContents = `ls ${__dirname}`;
             const expected = [
                 'index.js',
@@ -20,12 +22,14 @@ describe('vamtiger-bash should', function () {
                 'index.js.map'
             ];
             const test = __1.default(listDirectoryContents)
+                .then(bashResult => result = result)
                 .then(result => result.split('\n'))
                 .then(result => result.map(entry => entry.trim()))
                 .then(result => result.filter(entry => entry))
-                .tap(directoryContents => chai_1.expect(directoryContents).to.be.ok)
-                .tap(directoryContents => chai_1.expect(directoryContents.length).to.equal(3))
-                .tap(directoryContents => chai_1.expect(directoryContents.every(entry => expected.some(expectedEntry => entry === expectedEntry))).to.be.true);
+                .then(currentResult => directoryContents = currentResult)
+                .then(() => chai_1.expect(directoryContents).to.be.ok)
+                .then(() => chai_1.expect(directoryContents.length).to.equal(3))
+                .then(() => chai_1.expect(directoryContents.every(entry => expected.some(expectedEntry => entry === expectedEntry))).to.be.true);
             return test;
         });
     });
